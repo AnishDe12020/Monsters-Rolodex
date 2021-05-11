e6ceeaca69b3789a90eb692ef840e48542ad8c9a
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2021-05-11 16:20:00 +0530

    First Commit Added State Just some practice so far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React, { Component } from "react"
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      string: "Anish"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({string: "Hello World"})}>
+          Change Text
+        </button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
