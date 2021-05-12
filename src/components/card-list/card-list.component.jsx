cea1d9ce4d7b8cfa1085920369b05c192f5ade05
AnishDe12020 <anishde2007@gmail.com>
2021-05-12 12:01:04 +0530

    Added monster cards, images and email

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,19 @@
 import React from "react"
+
+import Card from "../card/card.component"
+
 import "./card-list.styles.css"
 
 const CardList = props => {
     console.log(props)
     return (
-        <div className="card-list">{props.children}</div>
+        <div className="card-list">
+            {
+                props.monsters.map(monster => (
+                    <Card key={monster.id} monster={monster} />
+                ))
+            }
+        </div>
     )
 }
 
