ae571ac3e62122bc4096b5fad92588e23cddcd50
AnishDe12020 <anishde2007@gmail.com>
2021-05-12 14:09:52 +0530

    Added Search Box to search for monsters

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     }
   }
 
@@ -20,9 +21,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => {
+      return (
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      ) 
+    })
+
     return (  
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input type="search" placeholder="Search Monsters"  onChange={e => { 
+          this.setState({ searchField: e.target.value })
+        }}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
