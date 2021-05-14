8231c90fa511d54804a3ec292f97243dabeb1d8e
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-05-14 14:46:50 +0530

    Small title change

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
 
     return (  
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>Monsters Rolodex 💀💀</h1>
         <SearchBox 
           placeholder="Search Monsters"
           handleChange={this.handleChange}
